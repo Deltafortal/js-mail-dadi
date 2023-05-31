972c9a89ca01c02148521c0ef824fa7441e5be57
Deltafortal <aldolucchetta21@gmail.com>
2023-05-31 15:34:06 +0200

    Added javascript (dadi)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 console.log('JS OK');
 
+// Prendi nel DOM gli elementi necessari
+
+
+// Genera un numero random da 1 a 6 per l'utente
+
+const dadoUtente = Math.floor(Math.random() * 6 + 1);
+const dadoCPU = Math.floor(Math.random() * 6 + 1);
+
+if (dadoUtente === dadoCPU) {
+    console.log('Pareggio')
+}
+else if (dadoUtente > dadoCPU) {
+    console.log ('Hai vinto')
+}
+else {
+    console.log ('Ha vinto il computer')
+}
+
+console.log('Il tuo risultato è ' + dadoUtente);
+console.log('Il risultato del computer è ' + dadoCPU);
--- mail-dadi.md
+++ mail-dadi.md
@@ -3,7 +3,9 @@
 - (BONUS) Prendi nel DOM gli elementi necessari
 - Genera un numero random da 1 a 6 per l'utente.
 - Genera un numero random da 1 a 6 per il computer.
-    - **SE** Il numero inserito dall'utente è più alto
+    - **SE** I due numeri sono uguali
+        - Pareggio.
+    - **ALTRIMENTI SE** Il numero inserito dall'utente è più alto
         - L'utente ha vinto.
     - **ALTRIMENTI** 
         - Il computer ha vinto 
