8e7d2e540719bd9d6dde2d384265d5ac9f290d75
Deltafortal <aldolucchetta21@gmail.com>
2023-05-31 16:56:15 +0200

    Added javascript (email)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,54 @@
 console.log('JS OK');
 
+
+/************************* Es Email ****************************/
+
+
+// Crea la lista di email
+
+const permessi = ['cristoforocolombo@gmail.com' , 'supermario@gmail.com' , 'jonsnow@libero.it' , 'gandalfgrigio@hootmail.com']
+
+
+
+// Chiedi all'utente la sua email e salvala
+
+const email = prompt('Inserisci la tua email per accedere ' , 'jonsnow@libero.it').trim();
+
+
+
+// Controlla se l'email inserita è presente nella lista
+
+let autorizzato;
+let message;
+
+for (let i = 0; i < permessi.length; i++){
+
+    let check = permessi[i];
+    
+    if (email === check){
+        autorizzato = true;
+        console.log(autorizzato);
+    }
+
+    check = permessi[i +1];
+}
+
+// Personalizza il messaggio
+
+if (autorizzato === true) {
+    messaggio = 'Sei nella lista, benvenuto!'
+}
+else {
+    messaggio = 'Non sei nella lista, non hai il permesso di entrare!'
+}
+
+console.log(messaggio);
+
+
+
+
+/* Esercizio DADI 
+
 // Prendi nel DOM gli elementi necessari
 
 
@@ -20,3 +69,5 @@ else {
 
 console.log('Il tuo risultato è ' + dadoUtente);
 console.log('Il risultato del computer è ' + dadoCPU);
+
+*/
